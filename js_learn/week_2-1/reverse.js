38775d12f04c06ecb2ca13cbaa8f52ebf2d0dbc6
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-13 10:28:28 +0900

    파일정리

---
+++ js_learn/week_2-1/reverse.js
@@ -0,0 +1,9 @@
+function reverse(str){
+  reverStr = "";
+  for (var i = str.length - 1;i >= 0; i --){
+    reverStr= reverStr + str.charAt(i);
+  }
+  return reverStr;
+
+}
+document.write("abs"*3);
