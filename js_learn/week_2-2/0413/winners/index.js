9cd20e88b5cedd43d25feb80f7a94e7d94a67ca9
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-14 23:54:50 +0900

    당첨자 랜덤 뽑기

--- js_learn/week_2-2/0413/winners/index.js
+++ js_learn/week_2-2/0413/winners/index.js
@@ -1,10 +1,18 @@
-let winners = [];
-let names = [];
 
-for (let i = 0; i < 8; i++) {
-  let random = Math.floor(Math.random() * names.length);
-  winners.push(names[random]);
-  names.concat(names.splice(random, 1));
-}
 
-console.log(winners);
+
+
+const btn = document.querySelector('.btn')
+btn.addEventListener('click',function(){
+  let names = ['a','b','c','d','e','f','g'];
+  let winners = [];
+  console.log('1')
+  for (let i = 0; i < 4; i++) {
+    let random = Math.floor(Math.random() * names.length);
+    winners.push(names[random])
+    names.concat(names.splice(random,1));
+  }
+  console.log(winners)
+  const p  = document.querySelector('.win')
+  p.innerHTML = winners
+})
