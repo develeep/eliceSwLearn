b12d69fda955b4cf1f3ede0d35bfb99fdeb36c79
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-18 16:42:29 +0900

    Class

--- js_learn/week_3-1/0418/5/index.js
+++ js_learn/week_3-1/0418/5/index.js
@@ -3,6 +3,9 @@ class Car {
     this.name = name;
     this.year = year;
   }
+  changeCar(carName){
+    return this.name = carName; 
+  }
 }
 
 let myCar1 = new Car("Ford", 2014);
@@ -10,4 +13,6 @@ let myCar2 = new Car("Audi", 2019);
 
 document.write(myCar1.name);
 document.write(myCar1.year);
-console.log(myCar2);
+console.log(myCar2.name);
+myCar2.changeCar('BMW')
+console.log(myCar2.name)
