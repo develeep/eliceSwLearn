13e63fe7868a3e82ad43744dbef4197add5834a9
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-18 11:55:41 +0900

    script module, callback()함수 사용

---
+++ js_learn/week_3-1/dayNight.js
@@ -0,0 +1,26 @@
+function night(){
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('body').style.color = 'white';
+  let as = document.querySelectorAll('a');
+  for(let i = 0;i<as.length;i++){
+    as[i].style.color = 'white';
+  }
+}
+// 내부 사용 함수
+function day(){
+  document.querySelector('body').style.backgroundColor = 'white';
+  document.querySelector('body').style.color = 'black';
+  let as = document.querySelectorAll('a');
+  for(let i = 0;i<as.length;i++){
+    as[i].style.color = 'black';
+  }
+}
+// export = 외부 사용 함수
+export function dayNight(time){
+  if(time === "night"){
+    night();
+  }
+  else if(time === "day"){
+    day();
+  }
+}
