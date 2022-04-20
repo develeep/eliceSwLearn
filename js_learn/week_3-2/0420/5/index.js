c8f12b57d590bf9f1edf19d05b8534ccfa5c0fd5
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-20 17:57:22 +0900

    isTriangle

--- js_learn/week_3-2/0420/5/index.js
+++ js_learn/week_3-2/0420/5/index.js
@@ -1,6 +1,8 @@
 function isTriangle(a, b, c) {
-  const area = null;
-  return null;
+  const semiperimeter = (a+b+c)/2;
+  const area = Math.sqrt(semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c));
+  if(Number.isNaN(area)) return false;
+  return area > 0;
 }
 
 console.log(isTriangle(2, 4, 6)); //false
