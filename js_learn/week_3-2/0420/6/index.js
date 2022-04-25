e792a36071a81bf3b5a655e94c12e362fc92c9b5
develeep <dltmdgns0508@gmail.com>
2022-04-25 11:10:01 +0900

    commit

---
+++ js_learn/week_3-2/0420/6/index.js
@@ -0,0 +1,8 @@
+//배열의 모든 요소가 square number(정수의 제곱)인지 확인하는 함수를 작성하십시오.
+const isSquare = (array) => array.length > 0 ? 
+array.every((num)=>Number.isInteger(Math.sqrt(num)))
+:undefined;
+
+console.log(isSquare([1, 4, 9, 81, 36, 1024]));
+
+module.exports = { isSquare };
