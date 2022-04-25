c699cc638d3dbc625ef2c095852152694b375977
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-04-25 16:47:18 +0900

    promise 이해

---
+++ js_learn/week_4-1/0425/4/index.js
@@ -0,0 +1,23 @@
+// 지시사항에 따라 코드를 작성합니다.
+const resolveWhenPositiveNumber = (num)=>{
+  return new Promise((resolve,reject)=>{
+    if(num>0){
+      resolve(`${num}은 양수입니다!`)
+    }
+    else{
+      reject(`${num}은 음수입니다!`)
+    }
+  })
+}
+
+
+
+
+
+
+// 아래의 숫자를 자유롭게 변경해 가며 실행해 보세요.
+// 물론 그대로 두어도 됩니다. 채점과는 무관합니다.
+const inputA = 1
+
+// 실행 혹은 제출을 위한 코드입니다. 지우거나 수정하지 말아주세요.
+module.exports = { inputs: [inputA], func: resolveWhenPositiveNumber }
