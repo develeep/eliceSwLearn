0fb8b28d4677113174beaf778625fcd30a34ae2c
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-05-03 21:00:14 +0900

    모듈 export

---
+++ learnNodejs/export_module/app.js
@@ -0,0 +1,4 @@
+const calc1 = require("./calc")
+const calc2 = require("./calc2")
+console.log(calc1.add(3,5))
+console.log(calc2.add(4,5))
---
+++ learnNodejs/export_module/calc2.js
@@ -0,0 +1,7 @@
+const calc = {
+  add : (a,b)=>a+b,
+  multiply : (a,b)=>a*b,
+  divide : (a,b)=>a/b,
+  minus : (a,b)=>a-b,
+}
+module.exports = calc
