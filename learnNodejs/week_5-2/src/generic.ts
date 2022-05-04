3f29f6efb0c2ddadce4b603194bb217501c7f06b
develeep <dltmdgns0508@gmail.com>
2022-05-04 17:55:01 +0900

    generic 실습

---
+++ learnNodejs/week_5-2/src/generic.ts
@@ -0,0 +1,12 @@
+const add = <T>(a:T,b:T):T=>{
+  if (typeof a==='boolean') {
+    return a||b
+  }
+  return <any>a+<any>b
+}
+console.log(add<number>(13, 15));
+console.log(add<string>("hell", "o"));
+console.log(add<boolean>(false, true));
+
+// error
+// console.log(add<number>(3, "5"));
