61156a20b017c0f8f564a8796e77aece10705de5
dltmdgns0508 <dltmdgns0508@gmail.com>
2022-05-11 13:09:15 +0900

    mongodb 실습

--- learnNodejs/week_6-1/express/myapp/routes/post.js
+++ learnNodejs/week_6-1/express/myapp/routes/post.js
@@ -4,11 +4,19 @@ const router = express.Router();
 router.get('/',(req,res)=>{
   res.render('post')
 })
-router.post('/',(req,res)=>{
+router.post('/',(req,res,next)=>{
   const name = req.body.name;
   const phone = req.body.phone;
   const date = req.body.date;
-  res.json({name:name,phone:phone,date:date});
+  // 웹 : 1대1 통신 => 1요청 1응답 통신 종료
+  // res.json({name:name,phone:phone,date:date});
+  next();
+})
+
+router.post('/',(req,res)=>{
+  // 호출한 경로로 재 접근
+  res.redirect('/expost')
+
 })
 
 module.exports = router
