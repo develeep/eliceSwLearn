ddaff110b1b3f41e958ea01b3a9f625815f7ec61
Lee <dltmdgns0508@gmail.com>
2022-06-06 13:12:48 +0900

    React 설치 및 환경 살펴보기 + 컴포넌트 만들어 보기

---
+++ react-learn/src/App.js
@@ -0,0 +1,48 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header() {
+  return (
+    <div>
+      <h1>
+        <a href="">Web</a>
+      </h1>
+    </div>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB!
+    </article>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
+}
+
+export default App;
