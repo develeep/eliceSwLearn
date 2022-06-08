9421f81abab800a92c4f7f1cab5d237d553e5485
Lee <dltmdgns0508@gmail.com>
2022-06-08 10:07:34 +0900

    리액트 CRA 연습

---
+++ react-CRA-learn/test/src/App.js
@@ -0,0 +1,11 @@
+import ProfileCard from "./components/ProfileCard";
+
+function App() {
+  return (
+    <>
+      <ProfileCard title="Hello" body="lorem ipsum" />
+    </>
+  );
+}
+
+export default App;
