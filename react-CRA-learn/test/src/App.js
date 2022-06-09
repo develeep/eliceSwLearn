8734f0856f2224d3fdbcb60fb47d6bcac934ec46
Lee <dltmdgns0508@gmail.com>
2022-06-09 21:45:30 +0900

    react useState, useEffect 공부

--- react-CRA-learn/test/src/App.js
+++ react-CRA-learn/test/src/App.js
@@ -1,9 +1,43 @@
-import ProfileCard from "./components/ProfileCard";
+import { useState } from 'react';
+import { Form, Button, ListGroup } from 'react-bootstrap';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [items, setItems] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setItems([...items, input]);
+    setInput('');
+  };
+  const deleteHandler = (e) => {
+    e.preventDefault()
+    const newItems = items.filter((el,index)=>index!== e.target.key)
+    setItems(newItems)
+  }
+
   return (
     <>
-      <ProfileCard title="Hello" body="lorem ipsum" />
+      <form onSubmit={submitHandler}>
+        <Form.Group className="mb-3">
+          <Form.Label>Enter a Todo</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter a todo"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Add
+        </Button>
+      </form>
+      <ListGroup>
+        {items.map((item,index) => (
+          <div key={index}><ListGroup.Item >{item}</ListGroup.Item><Button key={index} onClick={deleteHandler}>Delete</Button></div>
+        ))}
+      </ListGroup>
     </>
   );
 }
