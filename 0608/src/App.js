fe39fa54d29a1c088348591fc1caedf1f10bbaa1
Lee <dltmdgns0508@gmail.com>
2022-06-14 22:52:54 +0900

    styled 컴포넌트와 추가 기능 구현

--- 0608/src/App.js
+++ 0608/src/App.js
@@ -1,17 +1,22 @@
 import { Button, ButtonGroup } from '@mui/material';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 function Header(props) {
   return (
-    <div>
+    <header className={props.className}>
       <h1>
         <a href="#" onClick={props.onSelect}>
           WWW
         </a>
       </h1>
-    </div>
+    </header>
   );
 }
+
+const HeaderStyled = styled(Header)`
+  border-bottom: 1px solid gray;
+`;
 
 function Nav(props) {
   const list = props.data.map((e) => (
@@ -43,15 +48,38 @@ function Article(props) {
   );
 }
 
-function createHandler() {}
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          props.onCreate(e.target.title.value, e.target.body.value);
+        }}
+      >
+        <p>
+          <input name="title" type="text" placeholder="title"></input>
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
 
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(3);
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
-  ];
+  ]);
   console.log(mode, id);
   let content = null;
   if (mode === 'WELCOME') {
@@ -60,18 +88,31 @@ function App() {
     const topic = topics.filter((e) => e.id === id)[0];
     console.log(topic);
     content = <Article title={topic.title} body={topic.body}></Article>;
+  } else if (mode === 'CREATE') {
+    content = (
+      <Create
+        onCreate={(title, body) => {
+          const newTopic = { id: nextId, title, body };
+          setTopics((current) => {
+            const newTopics = [...current, newTopic];
+            return newTopics;
+          });
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
 
   return (
     <div>
-      <Header
+      <HeaderStyled
         onSelect={(e) => {
           e.preventDefault();
           alert('Header!');
           setMode('WELCOME');
           setId(null);
         }}
-      ></Header>
+      ></HeaderStyled>
       <Nav
         data={topics}
         onSelect={(id) => {
@@ -86,14 +127,16 @@ function App() {
         <Button
           variant="outlined"
           onClick={() => {
-            alert('create!');
+            setMode('CREATE');
           }}
         >
           create
         </Button>
         <Button variant="outlined">update</Button>
       </ButtonGroup>
-      <Button variant="outlined">delete</Button>
+      <Button variant="outlined" onClick={() => {}}>
+        delete
+      </Button>
     </div>
   );
 }
