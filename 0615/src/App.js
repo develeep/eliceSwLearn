810e2104dcb48dd51e49dd082251c7b379ac0dce
Lee <dltmdgns0508@gmail.com>
2022-06-15 13:25:03 +0900

    여러개의 경로를 하나의 라우트가 처리하도록 컨트롤

--- 0615/src/App.js
+++ 0615/src/App.js
@@ -1,55 +1,67 @@
 import { Button, ButtonGroup } from '@mui/material';
 import { useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useParams } from 'react-router-dom';
 import { Article } from './components/Article';
 import { Create } from './components/Create';
 import { HeaderStyled } from './components/HeaderStyled';
 import { Nav } from './components/Nav';
 import { Read } from './components/Read';
 
+function Control(props) {
+  const params = useParams();
+  const id = parseInt(params.id);
+  console.log(id);
+  let contextUI = null;
+  if (id) {
+    contextUI = (
+      <>
+        <Button variant="outlined">update</Button>
+        <Button variant="outlined">delete</Button>
+      </>
+    );
+  }
+  return (
+    <>
+      <Button component={Link} to="/create" variant="outlined">
+        create
+      </Button>
+      {contextUI}
+    </>
+  );
+}
+
 function App() {
-  const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
   const [nextId, setNextId] = useState(3);
   const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
   ]);
-  
-
 
   return (
     <div>
-      <HeaderStyled onSelect={handleHeader()}></HeaderStyled>
+      <HeaderStyled></HeaderStyled>
       <Nav data={topics} onSelect={HandleNav()}></Nav>
       <Routes>
-        <Route path='/' element={<Article title="Welcome" body="Hello, WEB!"></Article>} />
-        <Route path='create' element={(
-          <Create
-            onCreate={handleOnCreate()}
-          ></Create>
-        )} />
-        <Route path='/read/:id' element={<Read topics={topics} />} />
+        <Route
+          path="/"
+          element={<Article title="Welcome" body="Hello, WEB!"></Article>}
+        />
+        <Route
+          path="create"
+          element={<Create onCreate={handleOnCreate()}></Create>}
+        />
+        <Route path="/read/:id" element={<Read topics={topics} />} />
       </Routes>
       <br />
-      <ButtonGroup color="secondary" aria-label="medium secondary button group">
-        <Button
-          component={Link}
-          to="/create"
-          variant="outlined"
-          onClick={handleCreate()}
-        >
-          create
-        </Button>
-        <Button variant="outlined">update</Button>
-      </ButtonGroup>
-      <Button variant="outlined" onClick={handleDelete()}>
-        delete
-      </Button>
+
+      <Routes>
+        {['/', '/read/:id', 'update/:id'].map((path) => {
+          return <Route key={path} path={path} element={<Control />} />;
+        })}
+      </Routes>
     </div>
   );
-
-
 
   function handleOnCreate() {
     return (title, body) => {
@@ -64,7 +76,6 @@ function App() {
 
   function HandleNav() {
     return (id) => {
-      setMode('READ');
       setId(id);
     };
   }
@@ -78,20 +89,7 @@ function App() {
           return true;
         }
       });
-      setMode('WELCOME');
       setTopics(newTopics);
-    };
-  }
-
-  function handleCreate() {
-    return () => {
-      setMode('CREATE');
-    };
-  }
-
-  function handleHeader() {
-    return () => {
-      setMode('WELCOME');
     };
   }
 }
