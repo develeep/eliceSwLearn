2d6d506c0e307a1036c5fdcfa506b27513bab0f8
Lee <dltmdgns0508@gmail.com>
2022-06-29 11:25:16 +0900

    라우팅 연습 + db생성

--- 0629/src/App.js
+++ 0629/src/App.js
@@ -1,9 +1,45 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from "react-router-dom";
+
+function Header() {
+  return <header>
+    <h1>
+      <Link to="/">WEB</Link>
+    </h1>
+  </header>;
+}
+
+
+function Nav() {
+  return <nav>
+    <ol>
+      <li><Link to="/read/1">html</Link></li>
+    </ol>
+  </nav>
+}
+
+function Welcome(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
+
+function Read() {
+  return <article>
+    <h2>Read</h2>
+    Hello, Read
+  </article>
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>      
+      <Nav></Nav>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/read/:id" element={<Read />} />
+      </Routes>
     </div>
   );
 }
---
+++ 0629/src/Header.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return <Header>
+    <h1>
+      <Link to="/">WEB</Link>
+    </h1>
+  </Header>;
+}
+
+
+export default Header
